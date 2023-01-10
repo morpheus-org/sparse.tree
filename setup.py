e7e69ab779f18e3a1502990cf2a56e8a0b1f2771
cstyl <cstyl16@gmail.com>
2023-01-10 11:40:00 +0000

    Added licence to files

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
+"""
+ setup.py
+ 
+ EPCC, The University of Edinburgh
+ 
+ (c) 2023 The University of Edinburgh
+ 
+ Contributing Authors:
+ Christodoulos Stylianou (c.stylianou@ed.ac.uk)
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ 	http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
 try:
     from setuptools import setup
 except ImportError:
@@ -11,7 +33,7 @@ with open("sparse_tree/__init__.py") as f:
             exec(line, info)
             break
 
-setup_info = dict(
+setup(
     name="sparse_tree",
     version=info["__version__"],
     author="Christodoulos Stylianou",
@@ -31,5 +53,3 @@ setup_info = dict(
     packages=["sparse_tree"],
     install_requires=["scikit-learn", "ssgetpy", "pandas", "numpy", "matplotlib"],
 )
-
-setup(**setup_info)
