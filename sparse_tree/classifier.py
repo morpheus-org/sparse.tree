2af7ae7bb29ae55cd494e3b73a73140060bfb5d8
cstyl <cstyl16@gmail.com>
2023-05-25 14:42:27 +0100

    Updated DecisionTreeClassifier to extract the tree both in text and binary files/buffers

--- sparse_tree/classifier.py
+++ sparse_tree/classifier.py
@@ -25,8 +25,14 @@ from sklearn import tree, ensemble
 from collections import Counter
 import numpy as np
 import os
+import io
 
-from .utils import extract_array
+from .utils import (
+    extract_array,
+    write_array_to_bytes,
+    write_to_bytes,
+    build_str_sizelist,
+)
 
 
 class DecisionTreeClassifier(tree.DecisionTreeClassifier):
@@ -38,9 +44,9 @@ class DecisionTreeClassifier(tree.DecisionTreeClassifier):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def extract(self, filename, feature_names):
+    def extract(self, file, feature_names, format="bin"):
         """
-        Extracts the tree in a file.
+        Extracts the tree in a binary or text file/buffer.
 
         Parameters
         ----------
@@ -48,40 +54,86 @@ class DecisionTreeClassifier(tree.DecisionTreeClassifier):
             A path to the filename to extract the tree in.
         feature_names : list
             A list containing the name of each feature.
+        format : str
+            The file format to be used when storing the tree.
+            Options are [bin | txt] for binary and text formats.
         """
-        tree = self.tree_
 
         if isinstance(feature_names, list):
-            features_size = len(feature_names)
+            nfeatures = len(feature_names)
         elif isinstance(feature_names, np.ndarray):
-            features_size = feature_names.shape[0]
+            nfeatures = feature_names.shape[0]
         else:
             raise TypeError(
                 f"Type of feature_names ({type(feature_names)}) must be either list or numpy.ndarray"
             )
 
-        with open(filename, "w") as f:
-            # Write sizes
-            f.write("# Sizes (NFeatures, Nclasses, NodeCount, MaxDepth)\n")
-            f.write(str(features_size) + "\t")
-            f.write(str(self.n_classes_) + "\t")
-            f.write(str(tree.node_count) + "\t")
-            f.write(str(tree.max_depth) + "\n")
+        if isinstance(file, str):
+            if format == "bin":
+                with open(file, "wb") as f:
+                    self._extract_buffer(f, nfeatures, feature_names)
+            elif format == "txt":
+                with open(file, "w") as f:
+                    self._extract_text(f, nfeatures, feature_names)
+        elif isinstance(file, io.BufferedWriter):
+            self._extract_buffer(file, nfeatures, feature_names)
+        elif isinstance(file, io.TextIOWrapper):
+            self._extract_text(file, nfeatures, feature_names)
+        else:
+            raise TypeError(
+                f"file {type(file)} must be either a filename (str) or a binary buffer (io.BufferedWriter)!"
+            )
 
-            # Write Classes
-            extract_array(f, self.classes_, self.n_classes_, comment="Classes")
-            # Write Feature Names
-            extract_array(f, feature_names, features_size, comment="Feature Names")
+    def _extract_buffer(self, file, nfeatures, feature_names):
+        if not isinstance(file, io.BufferedWriter):
+            raise TypeError("file handler must be of type io.BufferedWriter!")
 
-            # Write tree data
-            extract_array(f, tree.children_left, tree.node_count, comment="Left")
-            extract_array(f, tree.children_right, tree.node_count, comment="Right")
-            extract_array(f, tree.threshold, tree.node_count, comment="Threshold")
-            extract_array(f, tree.feature, tree.node_count, comment="Feature")
+        tree = self.tree_
 
-            f.write("# Values\n")
-            for i in range(tree.node_count):
-                extract_array(f, tree.value[i, 0], self.n_classes_)
+        write_to_bytes(file, nfeatures)
+        write_to_bytes(file, self.n_classes_)
+        write_to_bytes(file, tree.node_count)
+        write_to_bytes(file, tree.max_depth)
+        write_array_to_bytes(file, self.classes_)
+        write_array_to_bytes(file, build_str_sizelist(feature_names))
+        write_array_to_bytes(file, feature_names)
+        write_array_to_bytes(file, tree.children_left)
+        write_array_to_bytes(file, tree.children_right)
+        write_array_to_bytes(file, tree.threshold, dtype=np.float64)
+        write_array_to_bytes(file, tree.feature)
+        write_array_to_bytes(file, tree.value, dtype=np.float64)
+
+    def _extract_text(self, file, nfeatures, feature_names):
+        tree = self.tree_
+
+        # Write sizes
+        file.write("# Sizes (NFeatures, Nclasses, NodeCount, MaxDepth)\n")
+        file.write(str(nfeatures) + "\t")
+        file.write(str(self.n_classes_) + "\t")
+        file.write(str(tree.node_count) + "\t")
+        file.write(str(tree.max_depth) + "\n")
+
+        # Write Classes
+        extract_array(file, self.classes_, self.n_classes_, comment="Classes")
+        # Write Feature Names Sizes
+        extract_array(
+            file,
+            build_str_sizelist(feature_names),
+            nfeatures,
+            comment="Feature Names Sizes",
+        )
+        # Write Feature Names
+        extract_array(file, feature_names, nfeatures, comment="Feature Names")
+
+        # Write tree data
+        extract_array(file, tree.children_left, tree.node_count, comment="Left")
+        extract_array(file, tree.children_right, tree.node_count, comment="Right")
+        extract_array(file, tree.threshold, tree.node_count, comment="Threshold")
+        extract_array(file, tree.feature, tree.node_count, comment="Feature")
+
+        file.write("# Values\n")
+        for i in range(tree.node_count):
+            extract_array(file, tree.value[i, 0], self.n_classes_)
 
     def evaluate(self, sample):
         """
--- sparse_tree/utils.py
+++ sparse_tree/utils.py
@@ -21,9 +21,51 @@
  limitations under the License.
 """
 
+import numpy as np
+import io
+
 
 def extract_array(writer, array, size, comment=None):
     if comment:
         writer.write("# " + comment + "\n")
     for i, entry in enumerate(array):
         writer.write(str(entry) + ("\t" if i < size - 1 else "\n"))
+
+
+def write_array_to_bytes(file_handler, array, dtype=np.int32):
+    if not isinstance(file_handler, io.BufferedWriter):
+        raise TypeError("file handler must be of type io.BufferedWriter")
+
+    if type(array) is np.ndarray:
+        # write to bytes
+        file_handler.write(np.array(array, dtype=dtype).tobytes())
+    elif isinstance(array, list):
+        for entry in array:
+            write_to_bytes(file_handler, entry)
+    else:
+        raise TypeError("Array type not supported for BinaryIO!")
+
+
+def write_to_bytes(file_handler, entry):
+    if not isinstance(file_handler, io.BufferedWriter):
+        raise TypeError("file handler must be of type io.BufferedWriter!")
+
+    if isinstance(entry, (int, np.int32, np.int64)):
+        # write as int to bytes
+        file_handler.write(int(entry).to_bytes(4, byteorder="little", signed=True))
+    elif isinstance(entry, str):
+        # write as str with encoding
+        file_handler.write(entry.encode("utf-8"))
+    else:
+        raise TypeError("Type of entry not supported for BinaryIO!")
+
+
+def build_str_sizelist(lstr):
+    if not isinstance(lstr, list):
+        raise TypeError("lstr must be a list!")
+
+    lsize = np.zeros(len(lstr), dtype=np.int32)
+    for i, entry in enumerate(lstr):
+        lsize[i] = len(entry)
+
+    return lsize
